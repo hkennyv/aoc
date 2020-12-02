edbbe7cb298f73ca6bcb295e7b84213d9c3e4a70
Kenny Huynh <hkennyv@gmail.com>
2020-12-02 01:03:24 -0800

    add docstrings for cargo doc

--- 2020/day01/src/main.rs
+++ 2020/day01/src/main.rs
@@ -1,7 +1,50 @@
+#![crate_name = "day01"]
+
+//! ## --- Day 1: Report Repair ---
+//!
+//! After saving Christmas five years in a row, you've decided to take a vacation at a nice resort on a tropical island. Surely, Christmas will go on without you.
+//!
+//! The tropical island has its own currency and is entirely cash-only. The gold coins used there have a little picture of a starfish; the locals just call them stars. None of the currency exchanges seem to have heard of them, but somehow, you'll need to find fifty of these coins by the time you arrive so you can pay the deposit on your room.
+//!
+//! To save your vacation, you need to get all fifty stars by December 25th.
+//!
+//! Collect stars by solving puzzles. Two puzzles will be made available on each day in the Advent calendar; the second puzzle is unlocked when you complete the first. Each puzzle grants one star. Good luck!
+//!
+//! Before you leave, the Elves in accounting just need you to fix your expense report (your puzzle input); apparently, something isn't quite adding up.
+//!
+//! Specifically, they need you to find the two entries that sum to 2020 and then multiply those two numbers together.
+//!
+//! For example, suppose your expense report contained the following:
+//!
+//! ```
+//! 1721
+//! 979
+//! 366
+//! 299
+//! 675
+//! 1456
+//! ```
+//!
+//! In this list, the two entries that sum to 2020 are 1721 and 299. Multiplying them together produces 1721 \* 299 = 514579, so the correct answer is 514579.
+//!
+//! Of course, your expense report is much larger. Find the two entries that sum to 2020; what do you get if you multiply them together?
+//!
+//! ## --- Part Two ---
+//!
+//! The Elves in accounting are thankful for your help; one of them even offers you a starfish coin they had left over from a past vacation. They offer you a second one if you can find three numbers in your expense report that meet the same criteria.
+//!
+//! Using the above example again, the three entries that sum to 2020 are 979, 366, and 675. Multiplying them together produces the answer, 241861950.
+//!
+//! In your expense report, what is the product of the three entries that sum to 2020?
+//!
+//!
+
 use std::cmp::Ordering;
 use std::collections::HashSet;
 use std::fs;
 
+/// The main function prints out the results for part1 and part2 of the day01
+/// AOC
 fn main() {
     let contents = fs::read_to_string("input.txt").unwrap();
     let input: Vec<i32> = contents
@@ -16,6 +59,7 @@ fn main() {
     println!("Part 2 result: {}\n", res_pt2);
 }
 
+/// The part1 function calculates the result for part1
 fn part1(input: &Vec<i32>) -> Result<i32, String> {
     let unique_inputs: HashSet<i32> = input.iter().cloned().collect();
 
@@ -32,6 +76,7 @@ fn part1(input: &Vec<i32>) -> Result<i32, String> {
     Err(String::from("no result :(\n"))
 }
 
+/// The part2 function calculates the result for part2
 fn part2(input: &Vec<i32>) -> Result<i32, String> {
     // clone and sort input
     let mut input = input.clone();
@@ -88,6 +133,7 @@ fn part2(input: &Vec<i32>) -> Result<i32, String> {
     }
 }
 
+/// utility function that helps to check index bounds
 fn assert_indices_havent_crossed(i1: usize, i2: usize, i3: usize) -> bool {
     (i1 < i2) && (i2 < i3)
 }

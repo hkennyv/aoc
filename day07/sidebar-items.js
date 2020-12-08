bc768b49b483950bd4726f1bc5017b2db48346e1
hkennyv <hkennyv@gmail.com>
2020-12-08 08:25:05 +0000

    Deploying to gh-pages from  @ aca66d5d8181fb44de1ca85b91b9f7fdbdaed2d5 🚀

--- day07/sidebar-items.js
+++ day07/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"fn":[["main",""]]});
+initSidebarItems({"fn":[["main",""],["parse_rule","parses a line into a rule. returns the bag and a set of bags that the main bag contains"],["part1",""]]});

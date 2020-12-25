aa48bc254f719d91864152ed8ec1da62a60d852c
hkennyv <hkennyv@gmail.com>
2020-12-25 01:33:03 +0000

    Deploying to gh-pages from  @ 371f82148ac45222b225744055e12a267be33c82 🚀

--- day17/sidebar-items.js
+++ day17/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"fn":[["main",""],["parse_input",""],["part1",""],["part2",""]],"mod":[["cube",""]],"type":[["Grid",""],["Layer",""],["Line","1-D type"]]});
+initSidebarItems({"fn":[["count_active",""],["count_active4d",""],["get_active_neighbors",""],["get_active_neighbors4d",""],["main",""],["parse_input",""],["part1","simulates the cube growing for ndim = 3"],["part2","simulates the cube growing for ndim = 4"],["simulate_cycle",""],["simulate_cycle4d",""]],"type":[["Grid",""],["Grid4d",""],["Layer",""],["Line","1-D type"]]});

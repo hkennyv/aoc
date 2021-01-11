4a22948bbcacfa2c8a6933e77e13f5c74c74bf39
hkennyv <hkennyv@gmail.com>
2021-01-11 06:38:29 +0000

    Deploying to gh-pages from  @ 2b3be22c573385ec6c449810f8683b83dbe6f826 🚀

--- implementors/core/convert/trait.From.js
+++ implementors/core/convert/trait.From.js
@@ -1,4 +1,3 @@
 (function() {var implementors = {};
-implementors["regex"] = [{"text":"impl&lt;'t&gt; From&lt;Match&lt;'t&gt;&gt; for Range&lt;usize&gt;","synthetic":false,"types":[]},{"text":"impl&lt;'t&gt; From&lt;Match&lt;'t&gt;&gt; for &amp;'t str","synthetic":false,"types":[]},{"text":"impl&lt;'t&gt; From&lt;Match&lt;'t&gt;&gt; for Range&lt;usize&gt;","synthetic":false,"types":[]}];
 implementors["regex_syntax"] = [{"text":"impl From&lt;Error&gt; for Error","synthetic":false,"types":[]},{"text":"impl From&lt;Error&gt; for Error","synthetic":false,"types":[]}];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
